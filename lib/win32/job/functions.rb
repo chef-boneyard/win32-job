e7a9822ca33907d58129d46d2ea6e56d3cf5b98f
Daniel Berger <djberg96@gmail.com>
2014-02-03 18:07:55 -0700

    Added functions to support the wait method.

--- lib/win32/job/functions.rb
+++ lib/win32/job/functions.rb
@@ -11,10 +11,14 @@ module Windows
     attach_function :AssignProcessToJobObject, [:handle, :handle], :bool
     attach_function :CloseHandle, [:handle], :bool
     attach_function :CreateJobObject, :CreateJobObjectA, [:pointer, :string], :handle
+    attach_function :CreateIoCompletionPort, [:handle, :handle, :uintptr_t, :dword], :handle
+    attach_function :GetQueuedCompletionStatus, [:handle, :pointer, :pointer, :pointer, :dword], :bool
     attach_function :IsProcessInJob, [:handle, :handle, :pointer], :bool
     attach_function :OpenProcess, [:dword, :bool, :dword], :handle
     attach_function :QueryInformationJobObject, [:handle, :int, :pointer, :dword, :pointer], :bool
+    attach_function :ResumeThread, [:handle], :dword
     attach_function :SetInformationJobObject, [:handle, :int, :pointer, :dword], :bool
     attach_function :TerminateJobObject, [:handle, :uint], :bool
+    attach_function :WaitForSingleObjectEx, [:handle, :dword, :bool], :dword
   end
 end
